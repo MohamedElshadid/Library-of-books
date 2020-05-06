1e55693afc3a1adf0c4552b0856d3e1c89dc2483
amiraITI2019 <amirareda.iti.intake40@gmail.com>
2020-05-06 16:44:42 +0200

    Edit in favourite Table unique(user,book)

--- app/Favourite.php
+++ app/Favourite.php
@@ -8,9 +8,6 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Favourite extends Model
 {
     use SoftDeletes;
-
-    protected $primaryKey = array('user_id', 'book_id');
-    public $incrementing = false;
     protected $fillable = ['user_id','book_id'];
 
 
--- database/migrations/2020_05_06_133719_create_favourites_table.php
+++ database/migrations/2020_05_06_133719_create_favourites_table.php
@@ -18,7 +18,8 @@ class CreateFavouritesTable extends Migration
             $table->integer('user_id')->foreign('user_id')->references('id')->on('users');
             $table->integer('book_id')->foreign('book_id')->references('id')->on('books');
             $table->timestamps();
-            $table->softDeletes('deleted_at', 0);	
+            $table->softDeletes('deleted_at', 0);
+            $table->unique(['user_id','book_id']);	
         });
     }
 
