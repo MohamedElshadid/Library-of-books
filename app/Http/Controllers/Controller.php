017e4f5b12aa8cf127ea0a08fbf945a55b6f1602
MohamedElshadid <elshadid_mohamed@yahoo.com>
2020-05-15 01:36:46 +0200

    middelware for admin and user

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -14,6 +14,7 @@ class Controller extends BaseController
     {
 
         $this->middleware('auth');
+        
 
     }
     
--- routes/web.php
+++ routes/web.php
@@ -11,47 +11,52 @@ use Illuminate\Support\Facades\Auth;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/Route::get('/', function () {
+*/
+Auth::routes();
+Route::get('/', function () {
     return view('auth.login');
 });
+Route::middleware('admin')->group(function (){
+    Route::get('chart-js','chartController@index');
+    Route::get('relatedBooks','BookController@related_books');
+    Route::get('/home', 'HomeController@index')->name('home');
+    Route::get('/home/{id}', 'HomeController@destroy')->name('homeDestroy');
+    Route::get('/CategoriesPage','CategoryController@index')->name('CategoriesPage');
+    //routes used in category & books  part (maryam)
+    
+    
+    //category routes
+    Route::resource('categories', 'CategoryController');
+    //list books in specific category 
+    Route::get('category/{id}','BookController@index');
+    //store book info in category 
+    
+    Route::post('store','BookController@store');
+    //delete book from category
+     Route::get('category/bookDestroy/{id}','BookController@destroy');
+     ###################################################
+    // Route::resource('users', 'UserController');
+    Route::get('users/{user}/edit', 'UserController@editAdmin')->name('users.editAdmin');
+    Route::patch('users/{user}/update', 'UserController@updateAdmin')->name('users.updateAdmin');
+    
+    Route::get('login/facebook', 'Auth\LoginController@redirectToProvider');
+    Route::get('login/facebook/callback', 'Auth\LoginController@handleProviderCallback');
+    
+    Route::get('/admins', 'UserController@showAdmin')->name('admins.showAdmin');
+    Route::get('users/{users}/removeAdmin', 'UserController@removeAdmin')->name('users.removeAdmin');
+    
+    Route::get('/users', 'UserController@showUser')->name('users.showUser');
+    Route::get('users/{users}/makeAdmin', 'UserController@makeAdmin')->name('users.makeAdmin');
+    
+    Route::get('users/{users}/deactivate', 'UserController@deactivate')->name('users.deactivate');
+    Route::get('users/{users}/activate', 'UserController@activate')->name('users.activate');
+});
 
-Route::get('chart-js','chartController@index');
-Route::get('relatedBooks','BookController@related_books');
-Auth::routes();
-Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/home/{id}', 'HomeController@destroy')->name('homeDestroy');
-Route::get('/CategoriesPage','CategoryController@index')->name('CategoriesPage');
-//routes used in category & books  part (maryam)
-
-
-//category routes
-Route::resource('categories', 'CategoryController');
-//list books in specific category 
-Route::get('category/{id}','BookController@index');
-//store book info in category 
-
-Route::post('store','BookController@store');
-//delete book from category
- Route::get('category/bookDestroy/{id}','BookController@destroy');
- ###################################################
-// Route::resource('users', 'UserController');
-Route::get('users/{user}/edit', 'UserController@editAdmin')->name('users.editAdmin');
-Route::patch('users/{user}/update', 'UserController@updateAdmin')->name('users.updateAdmin');
-
-Route::get('login/facebook', 'Auth\LoginController@redirectToProvider');
-Route::get('login/facebook/callback', 'Auth\LoginController@handleProviderCallback');
-
-Route::get('/admins', 'UserController@showAdmin')->name('admins.showAdmin');
-Route::get('users/{users}/removeAdmin', 'UserController@removeAdmin')->name('users.removeAdmin');
-
-Route::get('/users', 'UserController@showUser')->name('users.showUser');
-Route::get('users/{users}/makeAdmin', 'UserController@makeAdmin')->name('users.makeAdmin');
-
-Route::get('users/{users}/deactivate', 'UserController@deactivate')->name('users.deactivate');
-Route::get('users/{users}/activate', 'UserController@activate')->name('users.activate');
 
 // URL::asset('path/to/asset');
 
+Route::middleware('user')->group(function (){ 
+    Route::get('/userDashbord', 'HomeController@userIndex')->name('userHome');
 
-Route::get('/userDashbord', 'HomeController@userIndex')->name('userHome');
+});
 
