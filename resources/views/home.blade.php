35bcdced34ab6bf24e64c1fa0383beeeae5b7a4e
MohamedElshadid <elshadid_mohamed@yahoo.com>
2020-05-14 06:42:03 +0200

    Design

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,51 +3,44 @@
 @section('content')
 <div class="overlay"></div>
 <div class="users">
-    <div class="row justify-content-center" style="width:50%;">
-        <div class="col-md-8" style="z-index:718;">
+    <div class="justify-content-center" style="width:100%;">
+        <div style="z-index:718;position:relative">
             {{--------- Flash Session -------}}
             @if(Session::has('success'))
                 <div class="alert alert-success">
                     {{ Session::get('success') }}
                 </div>
             @endif
-<table>
-    <tr>
-    @forelse ($books as $book)
-    <td>
-    <div class="card" style="background-color:rgba(255, 255, 255, 0.7)  !important width:150px;;height:350px; margin-left:10px">
-        <div class="card-header">                       
-           <div>  <img src="<?php echo asset('storage/'.$book->cover)?>" style="width:150px;height:100px;border:2px solid black ;" />
-           </div> 
-            <a href="#"><h3> {{$book->title}}</h3> </a>
-            <h5>Author: {{$book->author}}</h5>
-            <h5>Price: {{$book->price}} $</h5> 
-            <h6>{{$book->available_copies}} Availble</h6>
-            </div>
-              
-            <div class="card-body">
-                <form action="" method="post">
-                    @csrf
-                    @method('delete')
-                        <a href="bookDestroy/{{$book->id}}" class="btn btn-danger">Delete</a>
-                    </form>
-            
+            <div class="row">
+                @forelse ($books as $book)
+                    <div class="col-md-3 mt-2">
+                        <div class="card" style="background-color:rgba(255, 255, 255, 0.7)  !important width:150px;;height:350px;width:70%">
+                            <div class="card-header">                       
+                            <div>  <img src="<?php echo asset('storage/'.$book->cover)?>" style="width:100%;height:100px;border:2px solid black ;" />
+                                <a href="#"><h3> {{$book->title}}</h3> </a>
+                                <h5>Author: {{$book->author}}</h5>
+                                <h5>Price: {{$book->price}} $</h5> 
+                                <h6>{{$book->available_copies}} Availble</h6>
+                            </div>
+                                
+                                <div class="card-body">
+                                    <form action="" method="post">
+                                        @csrf
+                                        @method('delete')
+                                            <a href="bookDestroy/{{$book->id}}" class="btn btn-danger">Delete</a>
+                                        </form>
+                                
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                @empty
+                    <p>No books yet !!!! </p>
+                @endforelse
             </div>
         </div>
-
     </div>
-    
-</td>
-  
-
-    @empty
-    <p>No books yet !!!! </p>
-@endforelse
-</tr>
-</table>         
 </div>
-</div>
-   
-
 @endsection
 
